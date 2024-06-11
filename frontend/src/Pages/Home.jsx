2ed30da0f446173fd4a130011e2372ec4a77ed9c
SatvikSaxena2 <satvik2saxena@gmail.com>
2024-06-11 23:58:05 +1000

    added NavBar and images

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeroSection from '../Components/HeroSection'
+const Home = () => {
+  return (
+    <div>
+        <HeroSection/>
+    </div>
+  )
+}
+
+export default Home
